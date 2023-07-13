887a5ba9ed16f60219f481cd67b2051ccf4b3d46
MarcMouries <Marc@Mouries.net>
2023-07-13 17:17:47 -0400

    new

---
+++ ticket_data.js
@@ -0,0 +1,51 @@
+var ticket_data = [
+    {
+        number: "CCT0001001",
+        email: "Albert.Abbott@example.com",
+        phone: "123-100-1001",
+        created_on: "07-11-2023 02:24:05 PM",
+        status: "LG01"
+    },
+    {
+        number: "CCT0001002",
+        email: "Barbara.Barrett@example.com",
+        phone: "123-100-1002",
+        created_on: "07-12-2023 02:24:05 PM",
+        status: "LG02"
+    },
+    {
+        number: "CCT0001003",
+        email: "Cindy.Campbell@example.com",
+        phone: "123-100-1003",
+        created_on: "07-13-2023 02:24:05 PM",
+        status: "LG99"
+    },
+    {
+        number: "CCT0001004",
+		email: "Frank.Fisher@example.com",
+		phone: "123-100-1006",
+        created_on: "07-13-2023 02:24:05 PM",
+        status: "LG99"
+	},
+    {
+        number: "CCT0001004",
+		email: "Lucy.Larson@example.com",
+		phone: "123-100-1012",
+        created_on: "07-13-2023 02:24:05 PM",
+        status: "LG01"
+	},
+    {
+        number: "CCT0001004",
+		email: "Susan.Sanders@example.com",
+		phone: "123-100-1019",
+        created_on: "07-13-2023 02:24:05 PM",
+        status: "LG01"
+	},
+    {
+        number: "CCT0001004",
+		email: "Tracey.Taylor@example.com",
+		phone: "123-100-1020",
+        created_on: "07-13-2023 02:24:05 PM",
+        status: "LG01"
+	},
+]
