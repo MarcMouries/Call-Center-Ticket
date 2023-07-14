948b1afb6c739661c2acc04c1c3fb8336053198e
MarcMouries <Marc@Mouries.net>
2023-07-14 13:55:28 -0400

    date format to standard ISO 8601

--- ticket_data.js
+++ ticket_data.js
@@ -3,42 +3,49 @@ var ticket_data = [
         number: "CCT0001001",
         email: "Albert.Abbott@example.com",
         phone: "123-100-1001",
-        created_on: "07-11-2023 02:24:05 PM",
+        created_on: "2023-07-11 02:24:05 PM",
         status: "LG01"
     },
     {
         number: "CCT0001002",
         email: "Barbara.Barrett@example.com",
         phone: "123-100-1002",
-        created_on: "07-12-2023 02:24:05 PM",
+        created_on: "2023-07-12 02:24:05 PM",
         status: "LG02"
     },
     {
         number: "CCT0001003",
         email: "Cindy.Campbell@example.com",
         phone: "123-100-1003",
-        created_on: "07-13-2023 02:24:05 PM",
+        created_on: "2023-07-13 02:24:05 PM",
         status: "LG99"
     },
     {
         number: "CCT0001004",
-		email: "Lucy.Larson@example.com",
-		phone: "123-100-1012",
-        created_on: "07-13-2023 02:24:05 PM",
+        email: "Lucy.Larson@example.com",
+        phone: "123-100-1012",
+        created_on: "2023-07-13 02:24:05 PM",
         status: "LG01"
-	},
+    },
     {
-        number: "CCT0001004",
-		email: "Susan.Sanders@example.com",
-		phone: "123-100-1019",
-        created_on: "07-13-2023 02:24:05 PM",
+        number: "CCT0001005",
+        email: "Oscar.Obrien@example.com",
+        phone: "123-100-1015",
+        created_on: "2023-07-13 02:24:05 PM",
         status: "LG01"
-	},
+    },
     {
-        number: "CCT0001004",
-		email: "Tracey.Taylor@example.com",
-		phone: "123-100-1020",
-        created_on: "07-13-2023 02:24:05 PM",
+        number: "CCT0001006",
+        email: "Susan.Sanders@example.com",
+        phone: "123-100-1019",
+        created_on: "2023-07-13 02:24:05 PM",
         status: "LG01"
-	},
+    },
+    {
+        number: "CCT0001007",
+        email: "Tracey.Taylor@example.com",
+        phone: "123-100-1020",
+        created_on: "2023-07-14 01:24:05 AM",
+        status: "LG99"
+    },
 ]
