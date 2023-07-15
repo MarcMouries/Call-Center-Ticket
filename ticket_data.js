33f7581d73d35bc725a8991b98abae4eb59275b9
MarcMouries <Marc@Mouries.net>
2023-07-15 11:13:44 -0400

    new times

--- ticket_data.js
+++ ticket_data.js
@@ -3,49 +3,49 @@ var ticket_data = [
         number: "CCT0001001",
         email: "Albert.Abbott@example.com",
         phone: "123-100-1001",
-        created_on: "2023-07-11 02:24:05 PM",
+        created_on: "2023-07-11 06:24:53 AM",
         status: "LG01"
     },
     {
         number: "CCT0001002",
         email: "Barbara.Barrett@example.com",
         phone: "123-100-1002",
-        created_on: "2023-07-12 02:24:05 PM",
+        created_on: "2023-07-12 07:48:19 AM",
         status: "LG02"
     },
     {
         number: "CCT0001003",
         email: "Cindy.Campbell@example.com",
         phone: "123-100-1003",
-        created_on: "2023-07-13 02:24:05 PM",
+        created_on: "2023-07-13 04:12:07 AM",
         status: "LG99"
     },
     {
         number: "CCT0001004",
         email: "Lucy.Larson@example.com",
         phone: "123-100-1012",
-        created_on: "2023-07-13 02:24:05 PM",
+        created_on: "2023-07-13 05:39:32 AM",
         status: "LG01"
     },
     {
         number: "CCT0001005",
         email: "Oscar.Obrien@example.com",
         phone: "123-100-1015",
-        created_on: "2023-07-13 02:24:05 PM",
+        created_on: "2023-07-13 06:55:14 AM",
         status: "LG01"
     },
     {
         number: "CCT0001006",
         email: "Susan.Sanders@example.com",
         phone: "123-100-1019",
-        created_on: "2023-07-13 02:24:05 PM",
+        created_on: "2023-07-13 10:17:21 AM",
         status: "LG01"
     },
     {
         number: "CCT0001007",
         email: "Tracey.Taylor@example.com",
         phone: "123-100-1020",
-        created_on: "2023-07-14 01:24:05 AM",
+        created_on: "2023-07-14 09:03:48 AM",
         status: "LG99"
     },
-]
+];
